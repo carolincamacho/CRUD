f28be69f9066cd2da805ba3627f83af452bdb210
carolincamacho <carolincamacho@javerianacali.edu.co>
2025-05-20 20:09:36 -0500

    clase_20_may_2025

--- main.py
+++ main.py
@@ -22,9 +22,33 @@ def crud():
              if str(item['id'])==id_eliminar:
                 usuarios.remove(item)
                 break
-        return redirect(url_for("crud"))      
+        return redirect(url_for("crud"))   #llamar al nombre de la función
    
-    return render_template("registro.html",usuarios=usuarios)
+    return render_template("registro.html",usuarios=usuarios)  #lista que entrega el html
+
+@app.route("/update/<int:id>",methods=["GET","POST"]) #ruta con parametros
+def update(id):
+    print(usuarios)
+    estudiante_editar=''
+    #TODO: Identificar el diccionario del usuario con id entrega
+
+    for diccionario in usuarios:
+        if diccionario ['id']==id:
+             estudiante_editar=diccionario
+             print("el estudiante a editar es: ", estudiante_editar)
+             break
+    if request.method=="POST":
+         #TODO Actualizar el diccionacio de estudiante,
+        estudiante_editar['nombre_usuario']=request.form.get("nombre")
+        estudiante_editar['correo_usuario']=request.form.get("correo")
+        return redirect(url_for("crud"))
+
+    if estudiante_editar =='':
+        return f"no existe el usuario con el id: {id}"
+    
+    return render_template('editar.html',estudiante_editar=estudiante_editar)
+    #return f"el id del uduario a actualizar es: {id}"
+
 
 if __name__=="main":
 	app.run(debug=True)
